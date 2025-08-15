78d96127b2e69a9fef013dbab8bde9992fc6b447
Edu1003 <edu.crecenzio.1003@gmail.com>
2025-08-15 10:48:15 -0300

    aprendendo_for

---
+++ src/Estrutura_de_repeticao_for.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Estrutura_de_repeticao_for {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        var ii = 2;
+        for (var i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+
+//        ele so para se digitar "EXIT"
+//        for (;;){
+//            System.out.println("Informe um nome:");
+//            var name = scanner.next();
+//
+//            if(name.equalsIgnoreCase("exit")) break;
+//
+//            System.out.println(name);
+//        }
+    }
+}
