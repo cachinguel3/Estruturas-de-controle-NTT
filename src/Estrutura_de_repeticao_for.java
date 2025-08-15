6f56fe90c8393cfe4e9b81e4f0d59d25396c1ca6
Edu1003 <edu.crecenzio.1003@gmail.com>
2025-08-15 10:49:00 -0300

    sem_scanner

--- src/Estrutura_de_repeticao_for.java
+++ src/Estrutura_de_repeticao_for.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Estrutura_de_repeticao_for {
 
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+        //var scanner = new Scanner(System.in);
         var ii = 2;
         for (var i = 1; i <= 100; i++) {
             System.out.println(i);
